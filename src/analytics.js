9ab5e67947599967fb93996ad28ee1127afc0888
DavidWells <hello@davidwells.io>
2019-05-14 16:11:13 -0700

    clean url

--- src/analytics.js
+++ src/analytics.js
@@ -3,6 +3,37 @@ import segmentPlugin from 'analytics-plugin-segment'
 import customerIoPlugin from 'analytics-plugin-customerio'
 import gtagManagerPlugin from 'analytics-plugin-google-tag-manager'
 
+// Temp functions for clean URL
+function canonicalUrl() {
+  if (typeof window === 'undefined') return
+  const tags = document.getElementsByTagName('link')
+  for (var i = 0, tag; tag = tags[i]; i++) {
+    if (tag.getAttribute('rel') === 'canonical') {
+      return tag.getAttribute('href')
+    }
+  }
+}
+
+function paramsClean(url, param) {
+  const search = (url.split('?') || [ , ])[1]
+  if (!search || search.indexOf(param) === -1) {
+    return url
+  }
+  // remove all utm params from URL search
+  const regex = new RegExp(`(\\&|\\?)${param}([_A-Za-z0-9"+=\.%]+)`, 'g')
+  const cleanSearch = `?${search}`.replace(regex, '').replace(/^&/, '?')
+  // replace search params with clean params
+  const cleanURL = url.replace(`?${search}`, cleanSearch)
+  // use browser history API to clean the params
+  return cleanURL
+}
+
+function currentUrl(search) {
+  const canonical = canonicalUrl()
+  if (!canonical) return window.location.href.replace(/#.*$/, '')
+  return canonical.match(/\?/) ? canonical : `${canonical}${search}`
+}
+
 const analytics = Analytics({
   plugins: [
     gtagManagerPlugin({
@@ -20,13 +51,17 @@ const analytics = Analytics({
       NAMESPACE: 'netlify-analytics-plugin',
       page: ({ payload }) => {
         const { properties, meta } = payload
+        const cleanUrl = currentUrl(properties.search)
+        console.log('cleanUrl', cleanUrl)
+        const urlNoUtm = paramsClean(cleanUrl, 'utm')
+        console.log('urlNoUtm', urlNoUtm)
         const data = {
           method: 'POST',
           headers: new Headers({ 'Content-Type': 'application/json' }),
           body: JSON.stringify({
             date: meta.timestamp || new Date().getTime(),
             name: properties.title || document.title,
-            url: properties.url,
+            url: urlNoUtm,
             website: 'functions'
           }),
         }
