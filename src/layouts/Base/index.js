b431242d5fcd2a6ae0a3fe5b994e26c8cfdde299
DavidWells <hello@davidwells.io>
2019-06-03 22:33:01 -0700

    service workers are the devil

--- src/layouts/Base/index.js
+++ src/layouts/Base/index.js
@@ -2,6 +2,14 @@ import React from 'react'
 import Icon from '../../components/Icon'
 import '../index.css'
 
+if (typeof navigator !== 'undefined' && navigator.serviceWorker) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (let registration of registrations) { // eslint-disable-line
+      registration.unregister()
+    }
+  })
+}
+
 export default class Base extends React.Component {
   componentDidMount () {
     Icon.loadSprite()
